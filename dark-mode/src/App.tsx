ea42a1b92b1ddcca81bd0462abbe3e4af09fa4bd
Aman <aman.ver9345@gmail.com>
2026-03-07 18:30:36 +0530

    completed card and learnt dark mode

--- dark-mode/src/App.tsx
+++ dark-mode/src/App.tsx
@@ -1,24 +1,27 @@
+import { ThemeProvider } from "next-themes";
 import "./index.css";
 import Home from "./pages/Home";
 
 const App = () => {
   return (
-    <div className="min-h-screen flex justify-center items-center w-full bg-white relative text-gray-800">
-  {/* Circuit Board - Light Pattern */}
-  <div
-    className="absolute inset-0 z-0 pointer-events-none"
-    style={{
-      backgroundImage: `
+    <div className="min-h-screen flex justify-center items-center w-full bg-white dark:bg-neutral-950 relative dark:text-neutral-400 text-gray-800">
+      {/* Circuit Board - Light Pattern */}
+      <div
+        className="absolute inset-0 z-0 pointer-events-none"
+        style={{
+          backgroundImage: `
         repeating-linear-gradient(0deg, transparent, transparent 19px, rgba(75, 85, 99, 0.08) 19px, rgba(75, 85, 99, 0.08) 20px, transparent 20px, transparent 39px, rgba(75, 85, 99, 0.08) 39px, rgba(75, 85, 99, 0.08) 40px),
         repeating-linear-gradient(90deg, transparent, transparent 19px, rgba(75, 85, 99, 0.08) 19px, rgba(75, 85, 99, 0.08) 20px, transparent 20px, transparent 39px, rgba(75, 85, 99, 0.08) 39px, rgba(75, 85, 99, 0.08) 40px),
         radial-gradient(circle at 20px 20px, rgba(55, 65, 81, 0.12) 2px, transparent 2px),
         radial-gradient(circle at 40px 40px, rgba(55, 65, 81, 0.12) 2px, transparent 2px)
       `,
-      backgroundSize: '40px 40px, 40px 40px, 40px 40px, 40px 40px',
-    }}
-  />
-  <Home />
-</div>
+          backgroundSize: "40px 40px, 40px 40px, 40px 40px, 40px 40px",
+        }}
+      />
+      <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+        <Home />
+      </ThemeProvider>
+    </div>
   );
 };
 
--- dark-mode/src/pages/Home.tsx
+++ dark-mode/src/pages/Home.tsx
@@ -1,15 +1,37 @@
-import image from "../assets/download.jpeg"
+import { useTheme } from "next-themes";
+import image from "../assets/download.jpeg";
 
 const Home = () => {
-  return (
-      <div className="flex flex-col items-center justify-items-start relative z-10 h-120 w-90 border border-neutral-100 rounded-xl bg-neutral-50 shadow-2xl">
-        <div className="h-50 w-80 m-4 mb-0"><img className="rounded-md" src={image}></img></div>
-        <div className="p-4 pt-0 font-roboto">
-        <h2 className="mb-4 text-lg font-bold">The Hashira: Sentinels of the Demon Slayer Corps</h2>
-        <p className="text-md">The Hashira are the nine most elite swordsmen in the Demon Slayer Corps. Each is a master of a unique Breathing Style, serving as the ultimate defense against Muzan Kibutsuji and his most powerful demons.</p>
-        </div>
-      </div>
-  )
-}
+  
+  const {theme, setTheme} = useTheme();
 
-export default Home
+  const handleThemeChange = () => {
+    setTheme(theme === "dark" ? "light" : "dark");
+  }
+
+  return (
+    <div className="flex flex-col items-center justify-items-start relative z-10 h-130 w-90 border border-neutral-50 dark:border-neutral-800 rounded-xl bg-neutral-100 dark:bg-neutral-900 shadow-2xl">
+      <div className="h-50 w-80 m-4 mb-0">
+        <img className="rounded-md" src={image}></img>
+      </div>
+      <div className="p-6 pt-0 font-roboto flex flex-col justify-center items-end">
+        <h2 className="mb-4 text-lg font-bold">
+          The Hashira: Sentinels of the Demon Slayer Corps
+        </h2>
+        <p className="text-md">
+          The Hashira are the nine most elite swordsmen in the Demon Slayer
+          Corps. Each is a master of a unique Breathing Style, serving as the
+          ultimate defense against Muzan Kibutsuji and his most powerful demons.
+        </p>
+        <button
+          onClick={handleThemeChange}
+          className="px-8 py-2 mt-3 bg-black dark:bg-white dark:text-black text-white rounded-full cursor-pointer"
+        >
+          Switch
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
